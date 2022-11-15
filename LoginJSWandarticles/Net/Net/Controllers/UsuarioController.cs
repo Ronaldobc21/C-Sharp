7c5960008e16ac00a11c805ac7539eb2c64d8321
Ronald Bellorín <76532094+Ronaldobc21@users.noreply.github.com>
2022-11-15 04:47:35 -0600

    third commit  - uploading files Login&Articles
    third commit  - uploading files Login&Articles

---
+++ LoginJSWandarticles/Net/Net/Controllers/UsuarioController.cs
@@ -0,0 +1,92 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using System.Security.Claims;
+using Newtonsoft.Json;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreYouTube.Models;
+
+
+
+
+
+namespace NetCoreYouTube.Controllers
+{
+
+    [ApiController] // estan son sus rutas toda api debe tener unas rutas 
+    [Route("usuario")]
+    public class UsuarioController : ControllerBase
+    {
+
+        public IConfiguration _configuration; // Aqui en esta variable vamos obtener toda la configuracion que nesesitamos
+        //esto es un constructor como sabemos que es constructor porque tiene que tener el mismo nombre que la clase 
+        public UsuarioController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpPost]
+        [Route("login")]
+        public dynamic IniciarSesion([FromBody] Object optData) // Los datos que va mandar el usuario va ser de tipo objeto
+        {
+            var data = JsonConvert.DeserializeObject<dynamic>(optData.ToString());
+
+
+            string user = data.usuario.ToString();
+            string password = data.password.ToString(); // Esto es lo que escribimos dentro del potsman 
+            // Vamos a obtener que usuario en base al Usuario 
+
+            Usuario usuario = Usuario.DB().Where(x => x.usuario == user && x.password == password).FirstOrDefault();
+
+            if (usuario == null)
+            { // Si el usuario no existe en la DB va retornar esto
+                return new
+                {
+                    sucess = false,
+                    message = "Crendenciales incorrectas",
+                    result = ""
+                };
+            }
+
+
+            var jwt = _configuration.GetSection("Jwt").Get<Jwt>(); // Con getsection sirve que estamos obteniendo los datos del appseting.json en otras palabras del Json
+
+            var claims = new[]//Aqui especificaremos todo lo que va incluir nuestro token
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, jwt.Subject),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
+                new Claim("id", usuario.idUsuario),
+                new Claim("usuario", usuario.usuario)
+
+            }; 
+
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt.Key)); // Osea nuestra contraseña que esta en Jwt models que es Key tiene que convertirse en bytes
+            var singIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                jwt.Issuer,
+                jwt.Audience,
+                claims,
+                expires: DateTime.Now.AddMinutes(4),
+                signingCredentials: singIn
+                
+                );
+
+
+            return new
+            {
+                success = true,
+                message = "exito",
+                result = new JwtSecurityTokenHandler().WriteToken(token)
+
+
+            };
+
+        }
+
+
+
+
+    }
+}
---
+++ LoginJSWandarticles/Net/Net/Models/Jwt.cs
@@ -0,0 +1,59 @@
+﻿using System.Security.Claims;
+
+namespace NetCoreYouTube.Models
+{
+    public class Jwt
+    {
+        public string Key { get; set; } // Aqui lo mandamos a llamar por medio de variables
+        public string Issuer { get; set; }
+
+        public string Audience { get; set; }
+
+        public string Subject { get; set; }
+
+
+        // si es correcto o no es correcto si es enviado no enviado el token 
+        public static dynamic validarToken(ClaimsIdentity identity) // identity es el parametro
+        {
+            try
+            {// como detecto si el token es valido o no con un if
+
+                if(identity.Claims.Count() == 0 )// si el identity en las propiedades claim no trae ningun claim
+
+                {
+                    return new
+                    {
+                        success = false,
+                        message = "Verificar si estas enviando un token valido",
+                        result = ""
+                    };
+                }
+                //en caso tenga datos el claim
+                // nesesito saber que usuario le pertenece al token identity que es el parametro
+
+                var id = identity.Claims.FirstOrDefault(x => x.Type ==  "id").Value; // Que me obtenga el valor id de UsuarioController
+
+
+                Usuario usuario = Usuario.DB().FirstOrDefault(x => x.idUsuario == id);
+                return new
+                {
+                    success =true, 
+                    message = "exito",
+                    result = usuario
+                };
+            
+            
+            }
+            catch(Exception ex) // si ocurre un error se captura aqui
+            {
+                return new
+                {
+                    success = false,
+                    message = "Catch: "+ ex.Message,
+                    result = ""
+                };
+            }
+        }
+    }
+  
+}
